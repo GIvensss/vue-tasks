530df213f2d234e9d225f627621acaee5a1ca058
Yehor Hrytsenko <egor.grits56@gmail.com>
2022-08-01 17:57:33 +0300

    Add navigation sidebar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -19,16 +19,52 @@
       v-model="isMenuOpened"
       app
       temporary>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h3">
+            Task 1
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Navigate within the app
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
 
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <template v-for="item in items" >
+          <router-link :to="item.redirectsTo" :key="item.title">
+          <v-list-item
+            link
+          >
+            <v-list-item-content>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          </router-link>
+        </template>
+
+      </v-list>
     </v-navigation-drawer>
   </div>
 </template>
 
 <script>
+import { LOGIN_PAGE, NEWS_PAGE, PROFILE_PAGE } from '@/config/constants';
+
 export default {
   name: 'Header',
   data() {
     return {
+      items: [
+        { title: 'Login', redirectsTo: LOGIN_PAGE },
+        { title: 'Profile', redirectsTo: PROFILE_PAGE },
+        { title: 'News', redirectsTo: NEWS_PAGE },
+      ],
       isMenuOpened: true,
     };
   },
