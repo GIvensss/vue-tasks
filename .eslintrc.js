834be21ef862440e4067a5d4b42374c2a101d61b
Yehor Hrytsenko <egor.grits56@gmail.com>
2022-08-02 10:10:16 +0300

    Add login view

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,6 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'import/extensions': 'off',
     'vue/multi-word-component-names': 'off',
+    'linebreak-style': 'off',
   },
 };
